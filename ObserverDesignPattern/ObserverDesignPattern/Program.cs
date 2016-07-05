09e169181572d99c3e8702fd6c9b65a78225da8a
Srinivas Venbakam Vengadam <srinive@microsoft.com>
2016-07-05 14:18:05 +0530

    observer pattern c#

---
+++ ObserverDesignPattern/ObserverDesignPattern/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern
+{
+    public class Model
+    {
+        private string _modelobj;
+        private List<Observer> _observer = new List<Observer>();
+        
+        public string getModel()
+        {
+            return _modelobj;
+        }
+        public void setModel(string _modelobj)
+        {
+            this._modelobj = _modelobj;
+            this.notifyStateChange();
+        }
+
+        public void attach(Observer observer)
+        {
+            _observer.Add(observer);
+        }
+
+        public void notifyStateChange()
+        {
+            foreach (Observer observer in _observer)
+            {
+                observer.update();
+            }
+        }
+    }
+
+    public abstract class Observer
+    {        
+        public Model model;
+        public abstract void update();
+    }
+
+    public class ConcreteObserver : Observer
+    {
+        public ConcreteObserver(Model model)
+        {
+            this.model = model;
+            base.model.attach(this);
+        }
+
+        public override void update()
+        {           
+            Console.WriteLine("Value changed to: "+ model.getModel());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Model _modelobj = new Model();
+            ConcreteObserver obj = new ConcreteObserver(_modelobj);
+            _modelobj.setModel("hi");
+            _modelobj.setModel("hi1");
+            Console.Read();
+        }
+    }
+}
